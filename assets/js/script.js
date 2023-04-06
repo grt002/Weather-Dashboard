1ba675d9858756b4c3e9a1dadd584861581796b0
Garrett <grt002@outlook.com>
2023-04-06 17:04:17 -0500

    Added fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,17 @@ const formEl = document.querySelector('#search');
 
 function handleSearch(event) {
     event.preventDefault();
+
     const city = event.target.elements.city.value;
     console.log(city);
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 formEl.addEventListener('submit', handleSearch);
