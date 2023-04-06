647e514b57b427cb94ebc30bf1b786ae92009f64
Garrett <grt002@outlook.com>
2023-04-06 17:26:45 -0500

    Added HTML elements for weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 const formEl = document.querySelector('#search');
 
+const apiKey = "5857003d2f4974079f0c443aac764ae3";
+
 function handleSearch(event) {
     event.preventDefault();
 
     const city = event.target.elements.city.value;
     console.log(city);
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${5857003d2f4974079f0c443aac764ae3}&units=metric')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
